6790ea1830b5d8aac9c36d898d78f04174ffe6a1
joshua loates <j.loates@lancaster.ac.uk>
2024-12-08 22:03:23 +0000

    did more result analysis, and tidied up comments

--- XGBoost.R
+++ XGBoost.R
@@ -1,7 +1,7 @@
 library(xgboost)
 library(caret)
 
-set.seed(500)
+set.seed(38623919) # keeps consistency in result
 
 df <- read.csv("cleaned_telco_churn_data.csv", header = TRUE, sep = ",")
 
@@ -10,8 +10,7 @@ View(content)
 hist(df$Churn) # to view the distribution of churn=0 vs churn=1
 
 # Here we do an 80/20 split of the train/test data
-trainIndex <- createDataPartition(df$Churn, p = .8, 
-                                  list = FALSE)
+trainIndex <- createDataPartition(df$Churn, p = .8, list = FALSE)
 
 # We split the dataframe into training and testing
 train_df = df[trainIndex,]
@@ -24,15 +23,11 @@ train_label  = train_df[,10]
 test_data = data.matrix(test_df[,-10])
 test_label = test_df[,10]
 
-# confirming distribution of Churn is the same in both sets
-hist(train_label) 
-hist(test_label)
-
 # We use XGBoost to create dense matrixes for the train and test data
 xgb_train = xgb.DMatrix(data = train_data, label = train_label)
 xgb_test = xgb.DMatrix(data = test_data, label=test_label)
 
-# Used for Evaluating the model as we train it
+# Used for evaluating the model as we train it
 watchlist = list(train=xgb_train, test = xgb_test)
 
 # Trains the model
@@ -42,15 +37,21 @@ xgb_model = xgb.train(data = xgb_train, max.depth = 3, watchlist=watchlist,
 pred <- predict(xgb_model, test_data) # result is currently not 0-1, but decimal
 prediction <- as.numeric(pred > 0.5)
 
-error <- mean(prediction != test_label)
-paste("test-error=", error)
 
 # amount of Churns=0 and Churns=1 just to compare to predicted amount
 length(test_label[test_label==0])
 length(test_label[test_label==1])
 
+# calculate average error of model
+error <- mean(prediction != test_label)
+paste("test-error=", error)
 
-# table version of confusion matrix for results
-table(test_label, prediction)
+# calculate other accuracy predictions
+paste("mean-squared-error=", mean((test_label - prediction)^2)); 
+paste("mean-absolute-error=", caret::MAE(test_label, prediction));
+paste("root-mean-squared-error=", caret::RMSE(test_label, prediction));
+
+# confusion matrix as a table
+table(test_label, prediction) 
 
 
